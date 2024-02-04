f03a0b1826b06bd8213d9ea382c2d60de934bfee
kapzduke <60543005+kapzduke@users.noreply.github.com>
2024-02-04 18:51:17 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,6 @@ Attribute.add("grapes");
 Attribute.add("silicon");
 Events.on(ContentInitEvent, e => {
   Vars.content.block("liverdustry-apple-plant").attributes.set(Attribute.get("apple"), 1);
-  Vars.content.block("liverdustry-banana-plant-mature").attributes.set(Attribute.get("banana"), 1);
+  Vars.content.block("liverdustry-banana-plant-ripe").attributes.set(Attribute.get("banana"), 1);
   Vars.content.block("liverdustry-silicon-plant").attributes.set(Attribute.get("silicon"), 1);
 });
